3509781e1bff7fc4fa85fa39233fe6eacc78515f
rirar <realhevay@gmail.com>
2026-09-09 08:05:43 +0700

    beta0.0

---
+++ kernel.c
@@ -0,0 +1,43 @@
+// main kernel
+// modul
+#include <sys/cdefs.h>
+
+
+struct __attribute__((packed)) multiboot_header {
+    unsigned int magic;
+    unsigned int flags;
+    unsigned int checksum;
+};
+
+__attribute__((section(".multiboot"), used, alighned(4)))
+const struct multiboot_header mb = {
+    0x1BADB002,
+    0x00000003,
+    -(0x1BADB002 + 0x00000003)
+};
+
+__attribute__((aligned(16)))
+unsigned char stack[16384];
+
+void kernel_main(void);
+
+__attribute__((naked))
+void _start(void) {
+    __asm__ volatile(
+        "cli\n\t"
+        "movl $stack+16384,%esp\n\t"
+        "call kernel_main\n"
+        "1: hlt\n\t"
+        "jmp 1b"
+    );
+}
+
+void kernel_main(void) {
+    volatile char *vga = (volatile char *)0xB8000;
+    const char *msg = "<<>>";
+
+    for (int i = 0; msg[i] != '\0'; i++) {
+        vga[i*2]       = msg[i];
+        vga[i * 2 + i] = 0x0f;
+    }
+}
