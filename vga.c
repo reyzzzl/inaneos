ae52d253709fbe50ea4ecaed5d622ab04cec502c
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-09-12 20:43:44 +0700

    fix port

--- vga.c
+++ vga.c
@@ -13,7 +13,7 @@ static void update_cursor(void){
     unsigned short pos = row * COLS + col;
     outb(0x3D4, 0x0F);
     outb(0x3D5, (unsigned char)(pos & 0xFF));
-    outb(0x3D5, 0x0E);
+    outb(0x3D4, 0x0E);
     outb(0x3D5, (unsigned char)(pos >> 8));
 }
 
